14ef4ecad583739d3f42004145bfbf9928e5337c
Beatriz Santos <beatrizsandp@gmail.com>
2019-05-25 12:16:33 -0300

    Link inserts

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 function randombg(){
     var random= Math.floor(Math.random() * 6) + 0;
-    var bigSize = ["url('./images/bg-01.jpg')",
-                   "url('./images/bg-02.jpg')",];
+    var bigSize = ["url('./images/bg-01.png')",
+                   "url('./images/bg-02.png')",];
     document.getElementById("random").style.backgroundImage=bigSize[random];
   }
 

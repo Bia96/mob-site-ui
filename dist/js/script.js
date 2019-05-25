57f6f3bb5a1327962f50dec979fc655dc9f2defd
Beatriz Santos <beatrizsandp@gmail.com>
2019-05-25 18:34:31 -0300

    Test C

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,8 @@
 
 function randombg(){
     var random= Math.floor(Math.random() * 6) + 0;
-    var bigSize = ["url('./images/bg-01.png')",
-                   "url('./images/bg-02.png')",];
+    var bigSize = ["url('images/bg-01.png')",
+                   "url('images/bg-02.png')",];
     document.getElementById("random").style.backgroundImage=bigSize[random];
     console.log
   }

da7820124458cfdd9e2f59505f39a46c626c22c4
Beatriz Santos <beatrizsandp@gmail.com>
2019-05-25 19:39:03 -0300

    Change background randomly

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,3 @@
   // Random landing pic function //
 
-function randombg(){
-    var random= Math.floor((Math.random() * 2) + 1);
-    var bigSize = ["url('dist/images/bg-01.png')",
-                   "url('dist/images/bg-02.png')",];
-    document.getElementById("random").style.backgroundImage=bigSize[random];
-    console.log(bigSize)
-  }
 

8c4de3e361c6113c7f2693511a448de2dfd9be15
Beatriz Santos <beatrizsandp@gmail.com>
2019-05-25 19:08:00 -0300

    Random bg function fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
   // Random landing pic function //
 
 function randombg(){
-    var random= Math.floor(Math.random() * 6) + 0;
+    var random= Math.floor((Math.random() * 2) + 1);
     var bigSize = ["url('dist/images/bg-01.png')",
                    "url('dist/images/bg-02.png')",];
     document.getElementById("random").style.backgroundImage=bigSize[random];
